bacf526d3a4155ac42784fbca1ef753108ed0ed7
Priya Dalal-Whelan <priyadw00@gmail.com>
2023-03-29 01:02:11 -0400

    validaton comments, maybe other small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
 
+    validates_presence_of :username
+    validates_pesence_of :password  
+
     def welcome
         "Hello, #{self.email}!"
     end
